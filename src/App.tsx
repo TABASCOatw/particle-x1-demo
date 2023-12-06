37ee53f0013a109d86c1a7bb5057d5d2db6f49d0
TABASCOatw <84655644+TABASCOatw@users.noreply.github.com>
2023-12-06 01:11:46 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react';
+
+import { useEthereum, useConnect, useAuthCore } from '@particle-network/auth-core-modal';
+import { X1Testnet } from '@particle-network/chains';
+import { AAWrapProvider, SmartAccount } from '@particle-network/aa';
+
+import { ethers } from 'ethers';
+import { notification } from 'antd';
+
+import './App.css';
+
+const App = () => {
+  const { provider } = useEthereum();
+  const { connect, disconnect } = useConnect();
+  const { userInfo } = useAuthCore();
+
+  const [balance, setBalance] = useState(null);
+
+  const smartAccount = new SmartAccount(provider, {
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    clientKey: process.env.REACT_APP_CLIENT_KEY,
+    appId: process.env.REACT_APP_APP_ID,
+    aaOptions: {
+      simple: [{ chainId: X1Testnet.id, version: '1.0.0' }]
+    }
+  });
+
+  const customProvider = new ethers.providers.Web3Provider(new AAWrapProvider(smartAccount), "any");
+
+  useEffect(() => {
+    if (userInfo) {
+      fetchBalance();
+    }
+  }, [userInfo]);
+
+  const fetchBalance = async () => {
+    const balanceResponse = await customProvider.getBalance(await smartAccount.getAddress());
+    setBalance(ethers.utils.formatEther(balanceResponse));
+  };
+
+  const handleLogin = async (authType) => {
+    if (!userInfo) {
+      await connect({
+        socialType: authType,
+        chain: X1Testnet,
+      });
+    }
+  };
+
+  const executeUserOp = async () => {
+    const signer = customProvider.getSigner();
+
+    const tx = {
+      to: "0x000000000000000000000000000000000000dEaD",
+      value: ethers.utils.parseEther("0.001"),
+    };
+
+    const txResponse = await signer.sendTransaction(tx);
+    const txReceipt = await txResponse.wait();
+
+    notification.success({
+      message: txReceipt.transactionHash
+    })
+  };
+
+  return (
+    <div className="App">
+      {!userInfo ? (
+        <div className="login-section">
+          <button className="sign-button" onClick={() => handleLogin('google')}>Sign in with Google</button>
+          <button className="sign-button" onClick={() => handleLogin('twitter')}>Sign in with Twitter</button>
+        </div>
+      ) : (
+        <div className="profile-card">
+          <h2>{userInfo.name}</h2>
+          <div className="balance-section">
+            <small>{balance} OKB</small>
+            <button className="sign-message-button" onClick={executeUserOp}>Execute User Operation</button>
+            <button className="disconnect-button" onClick={() => disconnect()}>Logout</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { AuthType } from '@particle-network/auth-core';
+import { BaseGoerli } from '@particle-network/chains';
+import { AuthCoreContextProvider } from '@particle-network/auth-core-modal';
+
+import App from './App';
+
+import('buffer').then(({ Buffer }) => {
+  window.Buffer = Buffer;
+});
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <AuthCoreContextProvider
+      options={{
+        projectId: process.env.REACT_APP_PROJECT_ID,
+        clientKey: process.env.REACT_APP_CLIENT_KEY,
+        appId: process.env.REACT_APP_APP_ID,
+        authTypes: [AuthType.email, AuthType.google, AuthType.twitter],
+        themeType: 'dark',
+        fiatCoin: 'USD',
+        language: 'en',
+        erc4337: {
+          name: "SIMPLE",
+          version: "1.0.0",
+        },
+        wallet: {
+          visible: true,
+          customStyle: {
+            supportChains: [BaseGoerli],
+          }
+        },
+      }}
+    >
+      <App />
+    </AuthCoreContextProvider>
+  </React.StrictMode>
+)
